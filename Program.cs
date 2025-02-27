275758ed6665d6292d20e718e8617bcbb44d1b5a
wiwer456 <67992786+wiwer456@users.noreply.github.com>
2025-02-27 20:29:37 +0300

    Add files via upload
    решение1

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿while (true)
+{
+    double answer = 0;
+    double answer2 = 0;
+    double total_answer = 0;
+    double total_answer2 = 0;
+    double with_stepen = 0;
+    double with_stepen_minus = 0;
+    double drobn_int = 0;
+
+    Console.WriteLine("Введите число:");
+    double user_int = Convert.ToDouble(Console.ReadLine());
+    Console.Clear();
+
+    Console.WriteLine("Введите систему счисления числа [от 2 до 9]:");
+    int number_system = Convert.ToInt32(Console.ReadLine());
+    while (number_system < 2 || number_system > 9)
+    {
+        Console.Clear();
+        Console.WriteLine("Недопустимый формат, попробуйте снова");
+        Console.WriteLine("Введите систему счисления числа [от 2 до 9]:");
+        number_system = Convert.ToInt32(Console.ReadLine());
+    }
+
+    //Блок целой части
+    string count = user_int.ToString();
+    int stepen_plus = 0;
+    for (int i = 0; i < count.Length; i++)
+    {
+        
+        //разделение чисел с конца
+        answer = Math.Round((user_int % Math.Pow(10, (i + 1))) / Math.Pow(10, i), 0);
+
+        //поправление погрешности из-за округления
+        if (answer - (user_int % Math.Pow(10, (i + 1))) / Math.Pow(10, i) >= 0.1) {
+            answer--;   
+        }
+
+        //умножение текущего числа на систему счисления, возведённую в степень разрядности 
+        with_stepen = answer * Math.Pow(number_system, stepen_plus);
+        //Console.WriteLine($"число: {answer} умноженное на {number_system} в степени {stepen} = {with_stepen}");
+
+        stepen_plus++;
+        total_answer += with_stepen;
+    }
+
+    //Блок дробной части
+    string[] drobn_int2_str = user_int.ToString().Split(',');
+    double drobn_int2 = Convert.ToDouble(drobn_int2_str[1]);
+    int stepen_minus = Convert.ToInt32(drobn_int2_str[1].Length) * -1;
+    for (int i = 0; i < drobn_int2_str[1].Length; i++)
+    {
+        answer2 = Math.Round((drobn_int2 % Math.Pow(10, (i + 1))) / Math.Pow(10, i), 0);
+        if (answer2 - (drobn_int2 % Math.Pow(10, (i + 1))) / Math.Pow(10, i) >= 0.1)
+        {
+            answer2--;
+        }
+        with_stepen_minus = answer2 * Math.Pow(number_system, stepen_minus);
+        //Console.WriteLine($"число: {answer2} умноженное на {number_system} в степени {stepen_minus} = {with_stepen_minus}");
+        
+        stepen_minus++;
+        total_answer += with_stepen_minus;
+    }
+
+    //Блок информации
+    Console.Clear();
+    Console.WriteLine($"Исходное число: {user_int} \nСистема счисления: {number_system} \n---\nРезультат: {total_answer}");
+
+    //Функционал выхода
+    Console.WriteLine("\nВыйти? \n1) да");
+    string u_answ = Console.ReadLine();
+    if (u_answ == "1") 
+    { 
+        break; 
+    }else 
+    {
+        Console.Clear(); 
+    }
+}
